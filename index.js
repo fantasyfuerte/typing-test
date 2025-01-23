f4948df0c018cc5d428f970eb611d6efc5629642
fuerte <leandrofuerte75@gmail.com>
2025-01-23 11:09:19 +0100

    Preparing the fucntions

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const $time = document.querySelector("time");
 const $paragraph = document.querySelector("p");
 const $input = document.querySelector("input");
+
+const INITIAL_TIME = 30;
+
+const text =
+  "The quick brown fox jumps over the lazy dog and then the dog jumps over the lazy fox right after the fox jumps over the lazy dog but then a frog jumps into the scene to see what happens";
+
+let words = [];
+let currentTime = INITIAL_TIME;
+
+initGame();
+initEvents();
+
+function initGame() {}
+function initEvents() {}
